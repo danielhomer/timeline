f228e0c89c6fcce063538f0553241aeac1e1fb42
Daniel Homer <daniel.homer@informa.com>
2013-09-30 17:24:04 +0100

    More ajax tweaks

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -18,7 +18,13 @@ jQuery(document).ready(function($) {
 				var i = 0;
 				$.each(this, function() {
 					if (i == 0) $(".timeline-item.more").remove();
-					$('#timeline').append('<li class="timeline-item '+ this['service'].toLowerCase() +'"><div class="left-margin"><img src="/images/'+ this['service'].toLowerCase() +'-32.png" alt="'+ this['service'].toLowerCase() +' logo" /></div><div class="right-margin"><p class="content">'+ this['content'].toLowerCase() +'</p><p class="byline"><span id="datetime">'+ this['time'] +'</span> via <a href="#" class="vialink">'+ this['service'] +'</a></p></div></li>');
+					if (this['service'] == 'end') {
+						$('#timeline').append('<li class="timeline-item '+ this['service'].toLowerCase() +'"><div class="left-margin"></div></li>');
+					} else if (this['service'] == 'more') {
+						$('#timeline').append('<li class="timeline-item '+ this['service'].toLowerCase() +'"><div class="left-margin"><img src="/images/'+ this['service'].toLowerCase() +'-32.png" alt="'+ this['service'] +' logo" /></div><div class="right-margin"><p class="content"><a id="timeline-more">Load more posts</a></p></div></li>')
+					} else {
+						$('#timeline').append('<li class="timeline-item '+ this['service'].toLowerCase() +'"><div class="left-margin"><img src="/images/'+ this['service'].toLowerCase() +'-32.png" alt="'+ this['service'].toLowerCase() +' logo" /></div><div class="right-margin"><p class="content">'+ this['content'].toLowerCase() +'</p><p class="byline"><span id="datetime">'+ this['time'] +'</span> via <a href="#" class="vialink">'+ this['service'] +'</a></p></div></li>');
+					}
 				});
 			});
 		});
